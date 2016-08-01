a669522bc16c0d949a21b95bc5a53ec4c07c9080
cgons <cgons@pcxchange.ca>
2016-08-01 11:07:06 -0400

    Loading of secret/env data configured.

--- app/core/apps.py
+++ app/core/apps.py
@@ -1,8 +1,23 @@
+import os
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+from dotenv import load_dotenv
 
 
 class CoreConfig(AppConfig):
     name = 'core'
+    SECRET_FILE_PATH = os.path.join(settings.BASE_DIR, '.env.secret')
 
     def ready(self):
-        print("-- RAN ONCE")
+        self._ensure_env_file_exists()
+        load_dotenv(self.SECRET_FILE_PATH)
+
+    def _ensure_env_file_exists(self):
+        secret_file_exists = os.path.isfile(self.SECRET_FILE_PATH)
+        if not secret_file_exists:
+            raise ImproperlyConfigured("""
+            Secret file (using python-dotenv):
+            .env.secret not found in base dir: {}
+            """.format(settings.BASE_DIR))
